fd549d6160d6dc2adb6434654e0c7895189568a9
Kaze aja <michaelagam03@gmail.com>
2024-11-08 06:26:53 +0800

    Create schema.js

---
+++ src/supports/schema.js
@@ -0,0 +1,39 @@
+const usersSchema = {
+  name: '',
+  registered: false,
+  registeredTime: '',
+  umur: '',
+  seri: '',
+  premium: false,
+  premiumTime: '',
+  banned: false,
+  bannedReason: '',
+  limit: 20,
+  kupon: 5,
+  uang: 1000,
+  hitCmd: 0,
+  notes: '',
+  lastClaim: '',
+  lastHour: '',
+  lastUang: '',
+  lastKupon: '',
+  afkTime: -1,
+  afkReason: ''
+};
+
+const chatsSchema = {
+  name: '',
+  welcome: true,
+  antilink: true,
+  mute: false,
+  viewOnce: true,
+  antiToxic: true,
+  description: '',
+  welcomeCaption: '',
+  byeCaption: ''
+};
+
+module.exports = {
+  usersSchema,
+  chatsSchema
+}
